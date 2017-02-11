c282d8b842342cde9822744dd21fac7d2a36fcbc
lukechampine <luke.champine@gmail.com>
2017-02-11 14:20:50 -0500

    fix benchmarks

--- few_test.go
+++ few_test.go
@@ -7,24 +7,27 @@ import (
 	"unsafe"
 )
 
-// BenchmarkCastSmall-4   	2000000000	         1.59 ns/op
+// BenchmarkCastSmall-4   	2000000000	         1.65 ns/op	       0 B/op	       0 allocs/op
 func BenchmarkCastSmall(b *testing.B) {
+	b.ReportAllocs()
 	var x int
 	for i := 0; i < b.N; i++ {
 		_ = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{uintptr(unsafe.Pointer(&x)), int(unsafe.Sizeof(x)), int(unsafe.Sizeof(x))}))
 	}
 }
 
-// BenchmarkCastLarge-4   	2000000000	         1.60 ns/op
+// BenchmarkCastLarge-4   	2000000000	         1.67 ns/op	       0 B/op	       0 allocs/op
 func BenchmarkCastLarge(b *testing.B) {
+	b.ReportAllocs()
 	var x [1000]int
 	for i := 0; i < b.N; i++ {
 		_ = *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{uintptr(unsafe.Pointer(&x)), int(unsafe.Sizeof(x)), int(unsafe.Sizeof(x))}))
 	}
 }
 
-// BenchmarkCastPtr-4     	 1000000	      1602 ns/op
+// BenchmarkCastPtr-4     	   1000000	         1686 ns/op	       0 B/op	       0 allocs/op
 func BenchmarkCastPtr(b *testing.B) {
+	b.ReportAllocs()
 	var x [1000]*int
 	for i := range x {
 		x[i] = new(int)
@@ -36,8 +39,9 @@ func BenchmarkCastPtr(b *testing.B) {
 	}
 }
 
-// BenchmarkCastPtr-4     	 1000000	      1602 ns/op
+// BenchmarkJSON-4        	    30000	        57213 ns/op	   12840 B/op	       8 allocs/op
 func BenchmarkJSON(b *testing.B) {
+	b.ReportAllocs()
 	var x [1000]*int
 	for i := range x {
 		x[i] = new(int)
